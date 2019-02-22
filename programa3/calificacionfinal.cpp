08a1b06fb76c7eeba240ce9c85cc18c5bed37e99
MikeFranco <mfranco_98@yahoo.com>
2019-02-22 11:03:38 -0600

    programa3 completed

--- programa3/calificacionfinal.cpp
+++ programa3/calificacionfinal.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+#include <stdlib.h>
 
 int instrucciones();
 bool validarCalif(string);
-int volverAIngresarCalificacion();
+float volverAIngresarCalificacion();
 float cambiarStringAInt(string);
 int validarLength(string);
-int calificacionFinal;
+float calificacionFinal(float, float, float);
 
 
 int main(){
   string calif1string, calif2string, calif3string;
   float calif1num, calif2num, calif3num;
+  system("clear");
   cout<<"\n\t\t¿Cuál es tu calificación final del semestre?\n\n";
   instrucciones();
 
@@ -25,12 +27,21 @@ int main(){
   cout<<"\n¿Cuál fue tu calficación del tercer parcial? "; cin>>calif3string;
   validarCalif(calif3string)==1 && validarLength(calif3string)==1 ? calif3num=cambiarStringAInt(calif3string) : calif3num=volverAIngresarCalificacion();
 
+  system ("clear");
+  cout<<"\n\n\t\t\tCalificacion del primer parcial "<<calif1num<<endl;
+  cout<<"\n\n\t\t\tCalificacion del segundo  parcial "<<calif2num<<endl;
+  cout<<"\n\n\t\t\tCalificacion del tercer parcial "<<calif3num<<endl;
 
+  float califFinal = calificacionFinal(calif1num, calif2num, calif3num);
+  califFinal < 6
+    ? cout<<"\nHaz reprobado el semestre con "<<califFinal<<" :( échale ganas en el recurse\n\n"
+    : cout<<"\n¡Felicidades, pasaste el semestre con "<<califFinal<<" :D\n\n";
 
 }
 
 int instrucciones(){
   cout<<"\n\t Para un buen funcionamiento del programa favor de:\n";
+  cout<<"--Ingresar la calificación únicamente con 2 decimales\n";
   cout<<"--Ingresar únicamente números, de lo contrario, volver a ingresar la calificacion\n\n\n";
 }
 
@@ -39,9 +50,12 @@ bool validarCalif(string calificacion){
   int valido=0;
 
   for(int i=0;i<calificacion.size();i++){
-    cout<<calificacion[i]<<endl;
-    if(isdigit(calificacion[i]) && calificacion[i]==".") {valido=1; break;}
-
+    if(isdigit(calificacion[i])) {
+      valido=1; break;
+      if(calificacion=="."){
+        valido=1; break;
+      }
+    }
   }
   valido==0 ? cout<<"El valor ingresado no es un valor aritmetico\n" : cout<<"";
   return valido;
@@ -50,7 +64,7 @@ bool validarCalif(string calificacion){
 
 int validarLength(string calificacion){
   bool bandera=0;
-  calificacion.length() <=2 ? bandera=1 : bandera=0;
+  calificacion.length() <=3 ? bandera=1 : bandera=0;
   bandera == 1 ? cout<<"" : cout<<"La calificación no puede tener más de 2 digitos, favor de ingresar uno menor\n";
   return bandera;
 
@@ -58,18 +72,19 @@ int validarLength(string calificacion){
 
 float cambiarStringAInt(string calificacion){
   float newCalif = atof(calificacion.c_str());
-  cout<<newCalif;
-  newCalif >= 11 ? newCalif=volverAIngresarCalificacion() : newCalif;
+  newCalif >= 11 || newCalif==0 ? newCalif=volverAIngresarCalificacion() : newCalif;
   return newCalif;
 
 }
 
-int volverAIngresarCalificacion(){
+float volverAIngresarCalificacion(){
+  cout<<"La calificacion no puede ser leída correctamente\n";
   string calif;
-  int calificacion;
+  float calificacion;
   bool bandera = 0;
   while(bandera == 0){
     cout<<"\nIngrese de nuevo la calificacion: "; cin>>calif;
+
     if(validarCalif(calif)==1 && validarLength(calif)==1) {
       calificacion=cambiarStringAInt(calif);
       bandera=1;
@@ -77,4 +92,12 @@ int volverAIngresarCalificacion(){
     } else cout<<"Favor de revisar las instrucciones\n";
   }
   return calificacion;
+}
+
+float calificacionFinal(float calif1, float calif2, float calif3){
+  float mayor = calif1;
+  mayor < calif2 ? mayor = calif2 : mayor;
+  mayor < calif3 ? mayor = calif3 : mayor;
+  return mayor;
+
 }
