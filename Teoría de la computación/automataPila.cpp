4a69def50618d9669157c2f32e1f1760cfa1266b
MikeFranco <mfranco_98@yahoo.com>
2019-11-05 20:23:08 -0600

    deleted one conditioning

--- Teoría de la computación/automataPila.cpp
+++ Teoría de la computación/automataPila.cpp
@@ -21,14 +21,14 @@ int main(){
         ? state = 3
         : validateNumber(word[i])
           ? state = 2
-          : state == 3
+          : error()
+      : state == 3
             ? validateLetter(word[i]) || validateNumber(word[i])
               ? state = 3
               : error()
             : state == 2
                 ? stateIn2()
-                : error()
-      : error();
+                : error();
 
   }
   cout<<"Así acaba el estado:"<<state<<endl;
