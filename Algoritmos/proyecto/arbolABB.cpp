788dd6a755db28dd4c79caf9c05cf8572d20f48b
MikeFranco <mfranco_98@yahoo.com>
2020-05-15 21:49:53 -0500

    deleted useless functions

--- Algoritmos/proyecto/arbolABB.cpp
+++ Algoritmos/proyecto/arbolABB.cpp
@@ -104,16 +104,12 @@ class ArbolABB {
    Nodo *raiz;
    Nodo *actual;
    int contador;
-   int altura;
 
   public:
    // Constructor y destructor b�sicos:
    ArbolABB() : raiz(NULL), actual(NULL) {}
-   ~ArbolABB() { Podar(raiz); }
    // Insertar en �rbol ordenado:
    void Insertar(const int dat);
-   // Borrar un elemento del �rbol:
-   void Borrar(const int dat);
    // Funci�n de b�squeda:
    bool Buscar(const int dat);
    // Comprobar si el �rbol est� vac�o:
@@ -122,35 +118,17 @@ class ArbolABB {
    bool EsHoja(Nodo *r) { return !r->derecho && !r->izquierdo; }
    // Contar n�mero de nodos:
    const int NumeroNodos();
-   const int AlturaArbol();
-   // Calcular altura de un int:
-   int Altura(const int dat);
    // Devolver referencia al int del nodo actual:
    int &ValorActual() { return actual->dato; }
    // Moverse al nodo raiz:
    void Raiz() { actual = raiz; }
    // Aplicar una funci�n a cada elemento del �rbol:
    void InOrden(void (*func)(int&) , Nodo *nodo=NULL, bool r=true);
-   void PreOrden(void (*func)(int&) , Nodo *nodo=NULL, bool r=true);
-   void PostOrden(void (*func)(int&) , Nodo *nodo=NULL, bool r=true);
   private:
    // Funciones auxiliares
-   void Podar(Nodo* &);
    void auxContador(Nodo*);
-   void auxAltura(Nodo*, int);
 };
 
-// Poda: borrar todos los nodos a partir de uno, incluido
-void ArbolABB::Podar(Nodo* &nodo)
-{
-   // Algoritmo recursivo, recorrido en postorden
-   if(nodo) {
-      Podar(nodo->izquierdo); // Podar izquierdo
-      Podar(nodo->derecho);   // Podar derecho
-      delete nodo;            // Eliminar nodo
-      nodo = NULL;
-   }
-}
 
 // Insertar un int en el �rbol ABB
 void ArbolABB::Insertar(const int dat)
@@ -178,63 +156,6 @@ void ArbolABB::Insertar(const int dat)
    else if(dat > padre->dato) padre->derecho = new Nodo(dat);
 }
 
-// Eliminar un elemento de un �rbol ABB
-void ArbolABB::Borrar(const int dat)
-{
-   Nodo *padre = NULL;
-   Nodo *nodo;
-   int aux;
-
-   actual = raiz;
-   // Mientras sea posible que el valor est� en el �rbol
-   while(!Vacio(actual)) {
-      if(dat == actual->dato) { // Si el valor est� en el nodo actual
-         if(EsHoja(actual)) { // Y si adem�s es un nodo hoja: lo borramos
-            if(padre) // Si tiene padre (no es el nodo raiz)
-               // Anulamos el puntero que le hace referencia
-               if(padre->derecho == actual) padre->derecho = NULL;
-               else if(padre->izquierdo == actual) padre->izquierdo = NULL;
-            delete actual; // Borrar el nodo
-            actual = NULL;
-            return;
-         }
-         else { // Si el valor est� en el nodo actual, pero no es hoja
-            // Buscar nodo
-            padre = actual;
-            // Buscar nodo m�s izquierdo de rama derecha
-            if(actual->derecho) {
-               nodo = actual->derecho;
-               while(nodo->izquierdo) {
-                  padre = nodo;
-                  nodo = nodo->izquierdo;
-               }
-            }
-            // O buscar nodo m�s derecho de rama izquierda
-            else {
-               nodo = actual->izquierdo;
-               while(nodo->derecho) {
-                  padre = nodo;
-                  nodo = nodo->derecho;
-               }
-            }
-            // Intercambiar valores de no a borrar u nodo encontrado
-            // y continuar, cerrando el bucle. El nodo encontrado no tiene
-            // por qu� ser un nodo hoja, cerrando el bucle nos aseguramos
-            // de que s�lo se eliminan nodos hoja.
-            aux = actual->dato;
-            actual->dato = nodo->dato;
-            nodo->dato = aux;
-            actual = nodo;
-         }
-      }
-      else { // Todav�a no hemos encontrado el valor, seguir busc�ndolo
-         padre = actual;
-         if(dat > actual->dato) actual = actual->derecho;
-         else if(dat < actual->dato) actual = actual->izquierdo;
-      }
-   }
-}
-
 // Recorrido de �rbol en inorden, aplicamos la funci�n func, que tiene
 // el prototipo:
 // void func(int&);
@@ -246,27 +167,6 @@ void ArbolABB::InOrden(void (*func)(int&) , Nodo *nodo, bool r)
    if(nodo->derecho) InOrden(func, nodo->derecho, false);
 }
 
-// Recorrido de �rbol en preorden, aplicamos la funci�n func, que tiene
-// el prototipo:
-// void func(int&);
-void ArbolABB::PreOrden(void (*func)(int&), Nodo *nodo, bool r)
-{
-   if(r) nodo = raiz;
-   func(nodo->dato);
-   if(nodo->izquierdo) PreOrden(func, nodo->izquierdo, false);
-   if(nodo->derecho) PreOrden(func, nodo->derecho, false);
-}
-
-// Recorrido de �rbol en postorden, aplicamos la funci�n func, que tiene
-// el prototipo:
-// void func(int&);
-void ArbolABB::PostOrden(void (*func)(int&), Nodo *nodo, bool r)
-{
-   if(r) nodo = raiz;
-   if(nodo->izquierdo) PostOrden(func, nodo->izquierdo, false);
-   if(nodo->derecho) PostOrden(func, nodo->derecho, false);
-   func(nodo->dato);
-}
 
 // Buscar un valor en el �rbol
 bool ArbolABB::Buscar(const int dat)
@@ -280,24 +180,6 @@ bool ArbolABB::Buscar(const int dat)
       else if(dat < actual->dato) actual = actual->izquierdo;
    }
    return false; // No est� en �rbol
-}
-
-// Calcular la altura del nodo que contiene el int dat
-int ArbolABB::Altura(const int dat)
-{
-   int altura = 0;
-   actual = raiz;
-
-   // Todav�a puede aparecer, ya que quedan nodos por mirar
-   while(!Vacio(actual)) {
-      if(dat == actual->dato) return altura; // int encontrado
-      else {
-         altura++; // Incrementamos la altura, seguimos buscando
-         if(dat > actual->dato) actual = actual->derecho;
-         else if(dat < actual->dato) actual = actual->izquierdo;
-      }
-   }
-   return -1; // No est� en �rbol
 }
 
 // Contar el n�mero de nodos
@@ -319,31 +201,8 @@ void ArbolABB::auxContador(Nodo *nodo)
    if(nodo->derecho)   auxContador(nodo->derecho);
 }
 
-// Calcular la altura del �rbol, que es la altura del nodo de mayor altura.
-const int ArbolABB::AlturaArbol()
-{
-   altura = 0;
-
-   auxAltura(raiz, 0); // Funci�n auxiliar
-   return altura;
-}
-
-// Funci�n auxiliar para calcular altura. Funci�n recursiva de recorrido en
-// postorden, el proceso es actualizar la altura s�lo en nodos hojas de mayor
-// altura de la m�xima actual
-void ArbolABB::auxAltura(Nodo *nodo, int a)
-{
-   // Recorrido postorden
-   if(nodo->izquierdo) auxAltura(nodo->izquierdo, a+1);
-   if(nodo->derecho)   auxAltura(nodo->derecho, a+1);
-   // Proceso, si es un nodo hoja, y su altura es mayor que la actual del
-   // �rbol, actualizamos la altura actual del �rbol
-   if(EsHoja(nodo) && a > altura) altura = a;
-}
-
 // Funci�n de prueba para recorridos del �rbol
-void Mostrar(int &d)
-{
+void Mostrar(int &d){
    cout << d << ",";
 }
 
@@ -356,56 +215,14 @@ int main(){
    ArbolABB ArbolInt;
 
    // Inserci�n de nodos en �rbol:
-   for (int i = 0; i < 100; i++){
+   for (int i = 0; i < cn; i++){
       ArbolInt.Insertar(lista[i]);
    }
-   
-
-   cout << "Altura de arbol " << ArbolInt.AlturaArbol() << endl;
 
    // Mostrar el �rbol en tres ordenes distintos:
    cout << "InOrden: ";
    ArbolInt.InOrden(Mostrar);
    cout << endl;
-   cout << "PreOrden: ";
-   ArbolInt.PreOrden(Mostrar);
-   cout << endl;
-   cout << "PostOrden: ";
-   ArbolInt.PostOrden(Mostrar);
-   cout << endl;
-
-   // Borraremos algunos elementos:
-   cout << "N nodos: " << ArbolInt.NumeroNodos() << endl;
-   ArbolInt.Borrar(5);
-   cout << "Borrar   5: ";
-   ArbolInt.InOrden(Mostrar);
-   cout << endl;
-   ArbolInt.Borrar(8);
-   cout << "Borrar   8: ";
-   ArbolInt.InOrden(Mostrar);
-   cout << endl;
-   ArbolInt.Borrar(15);
-   cout << "Borrar  15: ";
-   ArbolInt.InOrden(Mostrar);
-   cout << endl;
-   ArbolInt.Borrar(245);
-   cout << "Borrar 245: ";
-   ArbolInt.InOrden(Mostrar);
-   cout << endl;
-   ArbolInt.Borrar(4);
-   cout << "Borrar   4: ";
-   ArbolInt.InOrden(Mostrar);
-   ArbolInt.Borrar(17);
-   cout << endl;
-   cout << "Borrar  17: ";
-   ArbolInt.InOrden(Mostrar);
-   cout << endl;
-
-   // Veamos algunos par�metros
-   cout << "N nodos: " << ArbolInt.NumeroNodos() << endl;
-   cout << "Altura de 1 " << ArbolInt.Altura(1) << endl;
-   cout << "Altura de 10 " << ArbolInt.Altura(10) << endl;
-   cout << "Altura de arbol " << ArbolInt.AlturaArbol() << endl;
 
    cin.get();
    return 0;
