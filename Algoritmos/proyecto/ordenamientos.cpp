79f0123b131a4eeaf3275abf13ea7d0a71510087
MikeFranco <mfranco_98@yahoo.com>
2020-06-18 18:16:53 -0500

    ya basta freezeer

--- Algoritmos/proyecto/ordenamientos.cpp
+++ Algoritmos/proyecto/ordenamientos.cpp
@@ -41,7 +41,10 @@ void ordenamientoMezcla(int *, int, int);
 void mezclarMitades(int *, int, int, int);
 void ordenamientoArbolBinario();
 void ordenamientoRadix();
-void radixSort(int);
+void radixSort(int*, int);
+int getMax(int*, int);
+void countSort(int*, int, int);
+
 
 clock_t t_ini, t_fin;
 
@@ -505,90 +508,55 @@ void mezclarMitades(int *a, int low, int high, int mid)
   }
 }
 
-
 void ordenamientoRadix()
 {
   cout << "Se inicia el ordenamiento radix" << endl;
-  radixSort (n);
+  radixSort(lista, cn);
 }
 
-void radixSort(int n)
+// Get maximum value from array.
+int getMax(int arr[], int n)
 {
-    int front[10], rear[10];
- 
-    struct {
-        int info;
-        int next;
-    } node[NUMELTS];
-     
-    int exp, first, i, j, k, p, q, y;
-  
-  /* Inicializar una lista vinculada */
-    for (i = 0; i < n-1; i++)
-    {
-        node[i].info = lista[i];
-        node[i].next = i+1;
-    } /* fin del for */
-     
-    node[n-1].info = lista[n-1];
-    node[n-1].next = -1;
-    first = 0; /* first es la cabeza de la lista vinculada */
-     
-    for (k = 1; k < 5; k++) 
-    {
-    /* Suponer que tenemos números de cuatro dígitos */
-        for (i = 0; i < 10; i++)
-        {
-        /*Inicializar colas */
-            rear[i] = -1;
-            front[i] = -1;
-        } /*fin del for */
- 
-        /* Procesar cada elemento en la lista */
-        while (first != -1)
-        {
-            p = first;
-            first = node[first].next;
-            y = node[p].info;
-            /* Extraer el kâsimo dÁgito */
-            exp = pow(10, k-1); /* elevar 10 a la (k-1)ésima potencia */
-            j = (y/exp) % 10;
-            /* Insertar y en queue[j] */
-            q = rear[j];
-            if (q == -1)
-                front[j] = p;
-            else
-                node[q].next = p;
-            rear[j] = p;
-        } /*fin del while */
-      
-        /* En este punto, cada registro está en su cola basándose en el dígito k
-           Ahora formar una lista única de todos los elementos de la cola.
-           Encontrar el primer elemento. */
-        for (j = 0; j < 10 && front[j] == -1; j++);
-            ;
-        first = front[j];
-      
-        /* Vincular las colas restantes */
-        while (j <= 9)
-        {   /* Verificar si se ha terminado */
-            /*Encontrar el elemento siguiente */
-            for (i = j+1; i < 10 && front[i] == -1; i++);
-                ;
-            if (i <= 9)
-            {
-                p = i;
-                node[rear[j]].next = front[i];
-            } /* fin del if */
-            j = i;
-        } /* fin del while */
-        node[rear[p]].next = -1;
-    } /* fin del for */
-  
-    /* Copiar de regreso al archivo original */
-    for (i = 0; i < n; i++) 
-    {
-            lista[i] = node[first].info;
-            first = node[first].next;
-    } /*fin del for */
-} /* fin de radixsort*/
+  int max = arr[0];
+  for (int i = 1; i < n; i++)
+    if (arr[i] > max)
+      max = arr[i];
+  return max;
+}
+
+// Sort arr[] of size n using Radix Sort.
+void radixSort(int arr[], int n)
+{
+  int exp, m;
+  m = getMax(arr, n);
+
+  // Calling countSort() for digit at (exp)th place in every input.
+  for (exp = 1; m / exp > 0; exp *= 10)
+    countSort(arr, n, exp);
+}
+
+// Count sort of arr[].
+void countSort(int arr[], int n, int exp)
+{
+  // Count[i] array will be counting the number of array values having that 'i' digit at their (exp)th place.
+  int output[n], i, count[10] = {0};
+
+  // Count the number of times each digit occurred at (exp)th place in every input.
+  for (i = 0; i < n; i++)
+    count[(arr[i] / exp) % 10]++;
+
+  // Calculating their cumulative count.
+  for (i = 1; i < 10; i++)
+    count[i] += count[i - 1];
+
+  // Inserting values according to the digit '(arr[i] / exp) % 10' fetched into count[(arr[i] / exp) % 10].
+  for (i = n - 1; i >= 0; i--)
+  {
+    output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+    count[(arr[i] / exp) % 10]--;
+  }
+
+  // Assigning the result to the arr pointer of main().
+  for (i = 0; i < n; i++)
+    arr[i] = output[i];
+}
